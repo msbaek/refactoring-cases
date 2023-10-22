1538d2a949b7d29cd2de8cc6593c7debae6c049c
Myeongseok Baek <codetemplate@hanmail.net>
2023-10-22 19:53:36 +0900

    project created

---
+++ src/main/java/pe/msbaek/rfcases/RfCasesApplication.java
@@ -0,0 +1,13 @@
+package pe.msbaek.rfcases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfCasesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RfCasesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/msbaek/rfcases/RfCasesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.msbaek.rfcases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfCasesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
