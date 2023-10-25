5aa897ef49b8c6c5268b55ffb8077c0b70fe50d7
Myeongseok Baek <codetemplate@hanmail.net>
2023-10-25 16:19:19 +0900

    add step down rule

---
+++ src/test/java/pe/msbaek/rfcases/kt4u/SproutMethodAndStepDownRuleTest.java
@@ -0,0 +1,20 @@
+package pe.msbaek.rfcases.kt4u;
+
+import org.junit.jupiter.api.Test;
+
+public class SproutMethodAndStepDownRuleTest {
+    @Test
+    void isEventProduct() {
+        /**
+         * Step Down Rule
+         *
+         * 상품이 이벤트 상품인지 확인하기
+         * - 주문한 상품 목록을 읽어오기
+         * - 위 결과에서 상품번호만 추출하기(map)
+         *   - loop
+         *   - map -> long goods_no
+         *   - to list
+         * - 이 번호들이 이벤트 상품 목록에 포함되어 있는지 확인하기
+         */
+    }
+}
