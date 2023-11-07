02e7858a318e864e4a8aeb9e906361bdcaceac96
Myeongseok Baek <codetemplate@hanmail.net>
2023-11-07 16:50:26 +0900

    extract field

--- src/test/java/pe/msbaek/rfcases/kt4u/ExcelFormatter.java
+++ src/test/java/pe/msbaek/rfcases/kt4u/ExcelFormatter.java
@@ -5,12 +5,14 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class ExcelFormatter {
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter
+            .ofPattern("yyyy-MM-dd HH:mm:ss")
+            .withZone(ZoneId.systemDefault());
+
     static String formatDate(final Instant value) {
-        if (null == value) return null;
-        return DateTimeFormatter
-                .ofPattern("yyyy-MM-dd HH:mm:ss")
-                .withZone(ZoneId.systemDefault())
-                .format(value);
+        if (null == value)
+            return null;
+        return dateTimeFormatter.format(value);
     }
 
     static String formatUser(final String userName, final String userLoginId) {
