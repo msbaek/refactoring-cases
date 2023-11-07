9a1ae83d442807e1d21abe4f41158b4b90667adc
Myeongseok Baek <codetemplate@hanmail.net>
2023-11-07 16:56:46 +0900

    extract superclass

---
+++ src/test/java/pe/msbaek/rfcases/kt4u/ExcelDownload.java
@@ -0,0 +1,52 @@
+package pe.msbaek.rfcases.kt4u;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ExcelDownload {
+    public Workbook from(final List<ReadErrorPackingResponse> parcelPostList) {
+        final List<String[]> data = createExcelData(parcelPostList);
+        return createWorkbook(data);
+    }
+
+    private List<String[]> createExcelData(final List<ReadErrorPackingResponse> packingList) {
+        final List<String[]> data = new ArrayList<>();
+        data.add(header());
+
+        for (final ReadErrorPackingResponse packing : packingList) {
+            data.add(row(packing));
+        }
+        return data;
+    }
+
+    private Workbook createWorkbook(final List<String[]> data) {
+        final Workbook workbook = new HSSFWorkbook();
+        final Sheet sheet = workbook.createSheet(getSheetName());
+
+        int rowCount = 0;
+        for (final String[] rowData : data) {
+            final Row row = sheet.createRow(rowCount);
+            rowCount++;
+
+            int columnCount = 0;
+            for (final String columnData : rowData) {
+                final Cell cell = row.createCell(columnCount);
+                columnCount++;
+                cell.setCellValue(columnData);
+            }
+        }
+        return workbook;
+    }
+
+    protected abstract String getSheetName();
+
+    protected abstract String[] header();
+
+    protected abstract String[] row(ReadErrorPackingResponse packing);
+}
--- src/test/java/pe/msbaek/rfcases/kt4u/PackingExcelDownload.java
+++ src/test/java/pe/msbaek/rfcases/kt4u/PackingExcelDownload.java
@@ -1,56 +1,15 @@
 package pe.msbaek.rfcases.kt4u;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class PackingExcelDownload {
+public class PackingExcelDownload extends ExcelDownload {
     private static final String sheetName = "packings";
 
-    public Workbook from(final List<ReadErrorPackingResponse> parcelPostList) {
-        final List<String[]> data = createExcelData(parcelPostList);
-        return createWorkbook(data);
-    }
-
-    private List<String[]> createExcelData(final List<ReadErrorPackingResponse> packingList) {
-        final List<String[]> data = new ArrayList<>();
-        data.add(header());
-
-        for (final ReadErrorPackingResponse packing : packingList) {
-            data.add(row(packing));
-        }
-        return data;
-    }
-
-    private Workbook createWorkbook(final List<String[]> data) {
-        final Workbook workbook = new HSSFWorkbook();
-        final Sheet sheet = workbook.createSheet(getSheetName());
-
-        int rowCount = 0;
-        for (final String[] rowData : data) {
-            final Row row = sheet.createRow(rowCount);
-            rowCount++;
-
-            int columnCount = 0;
-            for (final String columnData : rowData) {
-                final Cell cell = row.createCell(columnCount);
-                columnCount++;
-                cell.setCellValue(columnData);
-            }
-        }
-        return workbook;
-    }
-
-    private String getSheetName() {
+    @Override
+    protected String getSheetName() {
         return sheetName;
     }
 
-    private String[] header() {
+    @Override
+    protected String[] header() {
         return new String[]{
                 "창고",
                 "포장상태",
@@ -68,7 +27,8 @@ public class PackingExcelDownload {
         };
     }
 
-    private String[] row(ReadErrorPackingResponse packing) {
+    @Override
+    protected String[] row(ReadErrorPackingResponse packing) {
         return new String[]{
                 packing.warehouseName(),
                 String.valueOf(packing.status().getDescription()),
