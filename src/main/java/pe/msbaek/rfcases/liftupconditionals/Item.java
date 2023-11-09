7ff06b5bb696c49e1313efbe58a101ee3aac6595
Myeongseok Baek <codetemplate@hanmail.net>
2023-11-09 17:54:53 +0900

    lift up condition - for Backstage

--- src/main/java/pe/msbaek/rfcases/liftupconditionals/Item.java
+++ src/main/java/pe/msbaek/rfcases/liftupconditionals/Item.java
@@ -18,6 +18,7 @@ public class Item {
 
     void updateItem() {
         boolean isAgedBrie = name.equals("Aged Brie");
+        boolean isBackstagePass = name.equals("Backstage passes to a TAFKAL80ETC concert");
         if (isAgedBrie) {
             if (quality < 50) {
                 quality = quality + 1;
@@ -28,42 +29,41 @@ public class Item {
                     quality = quality + 1;
                 }
             }
-        } else {
-            if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (quality > 0) {
-                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                        quality = quality - 1;
+        } else if (isBackstagePass) {
+            if (quality < 50) {
+                quality = quality + 1;
+                if (sellIn < 11) {
+                    if (quality < 50) {
+                        quality = quality + 1;
                     }
                 }
-            } else {
-                if (quality < 50) {
-                    quality = quality + 1;
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
+                if (sellIn < 6) {
+                    if (quality < 50) {
+                        quality = quality + 1;
                     }
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
+                }
+            }
+            sellIn = sellIn - 1;
+            if (sellIn < 0) {
+                quality = 0;
+            }
+        } else {
+            if (quality > 0) {
+                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                    quality = quality - 1;
                 }
             }
             if (!name.equals("Sulfuras, Hand of Ragnaros")) {
                 sellIn = sellIn - 1;
             }
             if (sellIn < 0) {
-                if (!name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (quality > 0) {
-                        if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                            quality = quality - 1;
-                        }
+                if (quality > 0) {
+                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
+                        quality = quality - 1;
                     }
-                } else {
-                    quality = 0;
                 }
             }
         }
     }
+
 }
