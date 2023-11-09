140f1596b7fa00a5671d686bbd7f78efe4fc9d39
Myeongseok Baek <codetemplate@hanmail.net>
2023-11-09 17:56:45 +0900

    lift up condition - for isSulfuras

--- src/main/java/pe/msbaek/rfcases/liftupconditionals/Item.java
+++ src/main/java/pe/msbaek/rfcases/liftupconditionals/Item.java
@@ -19,6 +19,7 @@ public class Item {
     void updateItem() {
         boolean isAgedBrie = name.equals("Aged Brie");
         boolean isBackstagePass = name.equals("Backstage passes to a TAFKAL80ETC concert");
+        boolean isSulfuras = name.equals("Sulfuras, Hand of Ragnaros");
         if (isAgedBrie) {
             if (quality < 50) {
                 quality = quality + 1;
@@ -47,23 +48,17 @@ public class Item {
             if (sellIn < 0) {
                 quality = 0;
             }
+        } else if (isSulfuras) {
         } else {
             if (quality > 0) {
-                if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                    quality = quality - 1;
-                }
+                quality = quality - 1;
             }
-            if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                sellIn = sellIn - 1;
-            }
+            sellIn = sellIn - 1;
             if (sellIn < 0) {
                 if (quality > 0) {
-                    if (!name.equals("Sulfuras, Hand of Ragnaros")) {
-                        quality = quality - 1;
-                    }
+                    quality = quality - 1;
                 }
             }
         }
     }
-
 }
