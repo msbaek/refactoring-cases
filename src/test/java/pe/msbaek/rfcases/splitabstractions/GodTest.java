0622e1ee0cc8d6fc89fccbdb07d929a0d5513984
Myeongseok Baek <codetemplate@hanmail.net>
2023-11-10 15:14:47 +0900

    dark magic(partial spy)

--- src/test/java/pe/msbaek/rfcases/splitabstractions/GodTest.java
+++ src/test/java/pe/msbaek/rfcases/splitabstractions/GodTest.java
@@ -1,20 +1,32 @@
 package pe.msbaek.rfcases.splitabstractions;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static pe.msbaek.rfcases.splitabstractions.Order.PaymentMethod.CARD;
 
+@ExtendWith(MockitoExtension.class)
 class GodTest {
-    private God god;
+    @Spy private God god;
 
     @Test
     void high() { // + 5 more tests like this
-        Order order = new Order();
-        order.setPaymentMethod(CARD);
+        doNothing().when(god).low(any(Order.class));
+        Order order = orderPaidByCard();
 
         String result = god.high(order);
 
         assertThat(result).isEqualTo("bonus");
+    }
+
+    private Order orderPaidByCard() {
+        Order order = new Order();
+        order.setPaymentMethod(CARD);
+        return order;
     }
 }
