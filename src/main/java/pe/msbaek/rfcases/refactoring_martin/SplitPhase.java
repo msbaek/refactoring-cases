d887fe515511e3e0d3e972b599b0bc11a2af0eb8
Myeongseok Baek <codetemplate@hanmail.net>
2023-11-12 21:14:20 +0900

    extract method - applyShipping

--- src/main/java/pe/msbaek/rfcases/refactoring_martin/SplitPhase.java
+++ src/main/java/pe/msbaek/rfcases/refactoring_martin/SplitPhase.java
@@ -39,6 +39,11 @@ public class SplitPhase {
     public double priceOrder(Product Product, int quantity, ShippingMethod shippingMethod) {
         double basePrice = Product.getBasePrice() * quantity;
         double discount = Math.max(quantity - Product.getDiscountThreshold(), 0) * Product.getBasePrice() * Product.getDiscountRate();
+        double price = applyShipping(basePrice, shippingMethod, quantity, discount);
+        return price;
+    }
+
+    private double applyShipping(double basePrice, ShippingMethod shippingMethod, int quantity, double discount) {
         double shippingPerCase = (basePrice > shippingMethod.getDiscountThreshold()) ? shippingMethod.getDiscountedFee() : shippingMethod.getFeePerCase();
         double shippingCost = quantity * shippingPerCase;
         double price = basePrice - discount + shippingCost;
