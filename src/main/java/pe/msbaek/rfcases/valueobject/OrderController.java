38abf79930d130c9292b2f134b6e4691336c227d
Myeongseok Baek <codetemplate@hanmail.net>
2023-12-13 16:37:40 +0900

    move instance method to value object

--- src/main/java/pe/msbaek/rfcases/valueobject/OrderController.java
+++ src/main/java/pe/msbaek/rfcases/valueobject/OrderController.java
@@ -5,43 +5,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 public class OrderController {
-    private boolean isNoShippingRequired(Shipping shipping) {
-        return "direct".equals(shipping.deliveryCode()) || "donation".equals(shipping.deliveryCode());
-    }
-
     public void check(HttpServletRequest request) {
         String DELIVERY_KIND_CD = request.getParameter("delivery").toString();
         Double DELIVERY_WON_PRICE = 0.0;
         Double DELIVERY_PRICE = 0.0;
         String CD_VAL = null;
         Shipping shipping = new Shipping(checkFreeShipping(), DELIVERY_KIND_CD);
-        final boolean isNoShippingRequired = isNoShippingRequired(shipping);
-        if (hasCost(isNoShippingRequired, shipping)) {
-            DELIVERY_KIND_CD = getDeliveryKindCode(shipping);
-            DELIVERY_WON_PRICE = getDeliveryWonPrice(shipping);
-            DELIVERY_PRICE = getDeliveryPrice(shipping);
-            CD_VAL = getCodeValue(shipping);
+        final boolean isNoShippingRequired = shipping.isNoShippingRequired();
+        if (shipping.hasCost(isNoShippingRequired)) {
+            DELIVERY_KIND_CD = shipping.getDeliveryKindCode();
+            DELIVERY_WON_PRICE = shipping.getDeliveryWonPrice();
+            DELIVERY_PRICE = shipping.getDeliveryPrice();
+            CD_VAL = shipping.getCodeValue();
         }
-    }
-
-    private String getCodeValue(Shipping shipping) {
-        return shipping.deliveryInfo().get("CD_VAL").toString();
-    }
-
-    private double getDeliveryPrice(Shipping shipping) {
-        return Double.parseDouble(shipping.deliveryInfo().get("DISP_PRICE").toString());
-    }
-
-    private double getDeliveryWonPrice(Shipping shipping) {
-        return Double.parseDouble(shipping.deliveryInfo().get("PRICE").toString());
-    }
-
-    private String getDeliveryKindCode(Shipping shipping) {
-        return shipping.deliveryInfo().get("DELIVERY_KIND_CD").toString();
-    }
-
-    private boolean hasCost(boolean isNoShippingRequired, Shipping shipping) {
-        return !isNoShippingRequired && null != shipping.deliveryInfo();
     }
 
     private Map checkFreeShipping() {
--- src/main/java/pe/msbaek/rfcases/valueobject/Shipping.java
+++ src/main/java/pe/msbaek/rfcases/valueobject/Shipping.java
@@ -3,4 +3,27 @@ package pe.msbaek.rfcases.valueobject;
 import java.util.Map;
 
 public record Shipping(Map deliveryInfo, String deliveryCode) {
+    String getCodeValue() {
+        return deliveryInfo().get("CD_VAL").toString();
+    }
+
+    double getDeliveryPrice() {
+        return Double.parseDouble(deliveryInfo().get("DISP_PRICE").toString());
+    }
+
+    double getDeliveryWonPrice() {
+        return Double.parseDouble(deliveryInfo().get("PRICE").toString());
+    }
+
+    String getDeliveryKindCode() {
+        return deliveryInfo().get("DELIVERY_KIND_CD").toString();
+    }
+
+    boolean hasCost(boolean isNoShippingRequired) {
+        return !isNoShippingRequired && null != deliveryInfo();
+    }
+
+    boolean isNoShippingRequired() {
+        return "direct".equals(deliveryCode()) || "donation".equals(deliveryCode());
+    }
 }
