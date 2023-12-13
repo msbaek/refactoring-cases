09350f8b2f8a48d1f36973b09abaead6b858deef
Myeongseok Baek <codetemplate@hanmail.net>
2023-12-13 17:05:32 +0900

    move instance method

--- src/main/java/pe/msbaek/rfcases/valueobject/UserCart.java
+++ src/main/java/pe/msbaek/rfcases/valueobject/UserCart.java
@@ -9,12 +9,6 @@ public class UserCart {
     private Shipping shipping;
 
     public double finalShippingCostKrw(final List<UserCoupon> userCoupons) {
-        return (double)Math.round(shipping.costKrw() - calculateCouponDiscounts(new UserCoupons(userCoupons)));
-    }
-
-    private float calculateCouponDiscounts(UserCoupons userCoupons1) {
-        return userCoupons1.userCoupons().stream()
-                .map(UserCoupon::discountRate)
-                .reduce(0f, Float::sum);
+        return (double)Math.round(shipping.costKrw() - new UserCoupons(userCoupons).calculateCouponDiscounts());
     }
 }
--- src/main/java/pe/msbaek/rfcases/valueobject/UserCoupons.java
+++ src/main/java/pe/msbaek/rfcases/valueobject/UserCoupons.java
@@ -3,4 +3,9 @@ package pe.msbaek.rfcases.valueobject;
 import java.util.List;
 
 public record UserCoupons(List<UserCoupon> userCoupons) {
+    float calculateCouponDiscounts() {
+        return userCoupons().stream()
+                .map(UserCoupon::discountRate)
+                .reduce(0f, Float::sum);
+    }
 }
