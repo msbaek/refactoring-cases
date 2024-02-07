1559e4f8d7318225a7bfd0307a4a733c21f2544e
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-07 17:36:15 +0900

    parrot - use known constant instead of passed argument(type)

--- src/main/java/pe/msbaek/rfcases/repeating_switch/NorwegianBlueParrot.java
+++ src/main/java/pe/msbaek/rfcases/repeating_switch/NorwegianBlueParrot.java
@@ -2,6 +2,6 @@ package pe.msbaek.rfcases.repeating_switch;
 
 public class NorwegianBlueParrot extends Parrot {
     public NorwegianBlueParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(type, numberOfCoconuts, voltage, isNailed);
+        super(ParrotTypeEnum.NORWEGIAN_BLUE, numberOfCoconuts, voltage, isNailed);
     }
 }
