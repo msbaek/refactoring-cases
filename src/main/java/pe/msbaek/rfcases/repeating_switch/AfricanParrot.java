8cd4322f6fcaa1eb654c2e9e9fb56dc5d78126fc
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-07 17:44:43 +0900

    parrot - replace type with constant and remove unreachable branches

--- src/main/java/pe/msbaek/rfcases/repeating_switch/AfricanParrot.java
+++ src/main/java/pe/msbaek/rfcases/repeating_switch/AfricanParrot.java
@@ -14,10 +14,6 @@ public class AfricanParrot extends Parrot {
 
     @Override
     public double getSpeed() {
-        return switch (type) {
-            case EUROPEAN -> getBaseSpeed();
-            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
-        };
+        return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
     }
 }
--- src/main/java/pe/msbaek/rfcases/repeating_switch/NorwegianBlueParrot.java
+++ src/main/java/pe/msbaek/rfcases/repeating_switch/NorwegianBlueParrot.java
@@ -14,10 +14,6 @@ public class NorwegianBlueParrot extends Parrot {
 
     @Override
     public double getSpeed() {
-        return switch (type) {
-            case EUROPEAN -> getBaseSpeed();
-            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
-        };
+        return (isNailed) ? 0 : getBaseSpeed(voltage);
     }
 }
