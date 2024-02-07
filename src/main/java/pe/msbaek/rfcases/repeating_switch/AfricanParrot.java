53e86195bff2e10c4c838d8de65fe64f58e39a37
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-07 17:42:31 +0900

    parrot - replace type with constant and remove unreachable branches

--- src/main/java/pe/msbaek/rfcases/repeating_switch/AfricanParrot.java
+++ src/main/java/pe/msbaek/rfcases/repeating_switch/AfricanParrot.java
@@ -1,16 +1,14 @@
 package pe.msbaek.rfcases.repeating_switch;
 
+import static pe.msbaek.rfcases.repeating_switch.ParrotTypeEnum.AFRICAN;
+
 public class AfricanParrot extends Parrot {
     public AfricanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, voltage, isNailed);
+        super(AFRICAN, numberOfCoconuts, voltage, isNailed);
     }
 
     @Override
     public String getCry() {
-        return switch (type) {
-            case EUROPEAN -> "Sqoork!";
-            case AFRICAN -> "Sqaark!";
-            case NORWEGIAN_BLUE -> voltage > 0 ? "Bzzzzzz" : "...";
-        };
+        return "Sqaark!";
     }
 }
--- src/main/java/pe/msbaek/rfcases/repeating_switch/EuropeanParrot.java
+++ src/main/java/pe/msbaek/rfcases/repeating_switch/EuropeanParrot.java
@@ -1,16 +1,14 @@
 package pe.msbaek.rfcases.repeating_switch;
 
+import static pe.msbaek.rfcases.repeating_switch.ParrotTypeEnum.EUROPEAN;
+
 public class EuropeanParrot extends Parrot {
     public EuropeanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
-        super(ParrotTypeEnum.EUROPEAN, numberOfCoconuts, voltage, isNailed);
+        super(EUROPEAN, numberOfCoconuts, voltage, isNailed);
     }
 
     @Override
     public String getCry() {
-        return switch (type) {
-            case EUROPEAN -> "Sqoork!";
-            case AFRICAN -> "Sqaark!";
-            case NORWEGIAN_BLUE -> voltage > 0 ? "Bzzzzzz" : "...";
-        };
+        return "Sqoork!";
     }
 }
