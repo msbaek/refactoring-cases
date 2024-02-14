419b64f95b307cf50950bc5ffaddf537646acd7a
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-14 17:15:53 +0900

    from-test-to-product move instance method

--- src/main/java/pe/msbaek/rfcases/tdd/FanClubProduct.java
+++ src/main/java/pe/msbaek/rfcases/tdd/FanClubProduct.java
@@ -1,7 +1,23 @@
 package pe.msbaek.rfcases.tdd;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class FanClubProduct {
     public void existingMethod() {
         System.out.println("existingMethod");
+    }
+
+    String formatMemo(String newMemo, LocalDateTime now, Long userId, String originalMemo) {
+        if (StringUtils.isBlank(newMemo)) return originalMemo;
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        return String.format("[%s][%d] %s\n",
+                now.format(formatter),
+                userId,
+                newMemo) + StringUtils.defaultString(originalMemo);
     }
 }
--- src/test/java/pe/msbaek/rfcases/tdd/FanClubProductTest.java
+++ src/test/java/pe/msbaek/rfcases/tdd/FanClubProductTest.java
@@ -1,14 +1,20 @@
 package pe.msbaek.rfcases.tdd;
 
-import org.apache.commons.lang3.StringUtils;
 import org.approvaltests.Approvals;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 class FanClubProductTest {
+    private FanClubProduct fanClubProduct;
+
+    @BeforeEach
+    void setUp() {
+        fanClubProduct = new FanClubProduct();
+    }
+
     @DisplayName("메모를 입력하면 기존 메모 앞에 메모 형식에 맞게 포맷팅하여 추가한다")
     @Test
     void formatMemo() {
@@ -19,22 +25,9 @@ class FanClubProductTest {
         String newMemo = "새로운 메모";
         LocalDateTime now = LocalDateTime.of(2020, 1, 2, 10, 11, 12);
         Long userId = 4321l;
-        String formattedMemo = formatMemo(newMemo, now, userId, originalMemo);
+        String formattedMemo = fanClubProduct.formatMemo(newMemo, now, userId, originalMemo);
 
         // then
         Approvals.verify(formattedMemo);
-    }
-
-    FanClubProduct fanClubProduct = new FanClubProduct();
-
-    private String formatMemo(String newMemo, LocalDateTime now, Long userId, String originalMemo) {
-        if (StringUtils.isBlank(newMemo)) return originalMemo;
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-        return String.format("[%s][%d] %s\n",
-                now.format(formatter),
-                userId,
-                newMemo) + StringUtils.defaultString(originalMemo);
     }
 }
