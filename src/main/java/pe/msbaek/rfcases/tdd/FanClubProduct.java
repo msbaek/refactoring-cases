bfcd6824daeadbf5ab579b9ec7dfba23b26f0d2b
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-14 17:03:43 +0900

    from-test-to-product add files

---
+++ src/main/java/pe/msbaek/rfcases/tdd/FanClubProduct.java
@@ -0,0 +1,7 @@
+package pe.msbaek.rfcases.tdd;
+
+public class FanClubProduct {
+    public void existingMethod() {
+        System.out.println("existingMethod");
+    }
+}
---
+++ src/test/java/pe/msbaek/rfcases/tdd/FanClubProductTest.java
@@ -0,0 +1,7 @@
+package pe.msbaek.rfcases.tdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FanClubProductTest {
+
+}
