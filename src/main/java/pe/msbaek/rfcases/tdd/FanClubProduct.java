56b4f5d892267f5d66d5c0a43804a49bb4adfee1
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-14 17:16:35 +0900

    from-test-to-product extract to fields

--- src/main/java/pe/msbaek/rfcases/tdd/FanClubProduct.java
+++ src/main/java/pe/msbaek/rfcases/tdd/FanClubProduct.java
@@ -6,14 +6,14 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class FanClubProduct {
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     public void existingMethod() {
         System.out.println("existingMethod");
     }
 
     String formatMemo(String newMemo, LocalDateTime now, Long userId, String originalMemo) {
         if (StringUtils.isBlank(newMemo)) return originalMemo;
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
         return String.format("[%s][%d] %s\n",
                 now.format(formatter),
