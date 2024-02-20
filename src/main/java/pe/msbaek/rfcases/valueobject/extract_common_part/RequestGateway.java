d3f85da5538b9d03b3ffcf939809a1c33b59884b
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-20 15:17:21 +0900

    extract-common-parts - extract method for remove dup.(with keep original signature)

--- src/main/java/pe/msbaek/rfcases/valueobject/extract_common_part/RequestGateway.java
+++ src/main/java/pe/msbaek/rfcases/valueobject/extract_common_part/RequestGateway.java
@@ -8,21 +8,21 @@ interface KoreaPostDelivery {
 public class RequestGateway {
     private void extracted(final KoreaPostDelivery koreaPostDelivery, final Map<String, String> waybillRequestPairs) {
         if(false) {
-            if (0 < createVat(koreaPostDelivery).length()) {
-                if ("IM".equals(createVat(koreaPostDelivery).substring(0, 2)) || "GB".equals(createVat(koreaPostDelivery).substring(0, 2))) {
-                    waybillRequestPairs.put("currunitcd", "USD");
-                }
-            }
+            extracted1(koreaPostDelivery, waybillRequestPairs);
         } else {
             if (0 < createVat(koreaPostDelivery).length()) {
                 if ("IM".equals(createVat(koreaPostDelivery).substring(0, 2)) || "GB".equals(createVat(koreaPostDelivery).substring(0, 2))) {
                     waybillRequestPairs.put("vatdscrnno", createVat(koreaPostDelivery));
                 }
             }
-            if (0 < createVat(koreaPostDelivery).length()) {
-                if ("IM".equals(createVat(koreaPostDelivery).substring(0, 2)) || "GB".equals(createVat(koreaPostDelivery).substring(0, 2))) {
-                    waybillRequestPairs.put("currunitcd", "USD");
-                }
+            extracted1(koreaPostDelivery, waybillRequestPairs);
+        }
+    }
+
+    private void extracted1(KoreaPostDelivery koreaPostDelivery, Map<String, String> waybillRequestPairs) {
+        if (0 < createVat(koreaPostDelivery).length()) {
+            if ("IM".equals(createVat(koreaPostDelivery).substring(0, 2)) || "GB".equals(createVat(koreaPostDelivery).substring(0, 2))) {
+                waybillRequestPairs.put("currunitcd", "USD");
             }
         }
     }
