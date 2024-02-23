f0dbd43cc541b1dfb2ea7d22777deff683a8cd0c
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-24 08:18:41 +0900

    usage first - make it compile(stubbing)

---
+++ src/main/java/pe/msbaek/rfcases/tdd/usagefirst/BasketItem.java
@@ -0,0 +1,8 @@
+package pe.msbaek.rfcases.tdd.usagefirst;
+
+import java.math.BigDecimal;
+
+public class BasketItem {
+    public BasketItem(String itemName, BigDecimal price) {
+    }
+}
---
+++ src/main/java/pe/msbaek/rfcases/tdd/usagefirst/ShoppingBasket.java
@@ -0,0 +1,17 @@
+package pe.msbaek.rfcases.tdd.usagefirst;
+
+import java.math.BigDecimal;
+
+public class ShoppingBasket {
+    public void add(BasketItem item, int quantity) {
+        throw new UnsupportedOperationException("ShoppingBasket#add Not implemented yet");
+    }
+
+    public int getQuantity(String itemName) {
+        throw new UnsupportedOperationException("ShoppingBasket#getQuantity Not implemented yet");
+    }
+
+    public BigDecimal calculateTotal() {
+        throw new UnsupportedOperationException("ShoppingBasket#calculateTotal Not implemented yet");
+    }
+}
