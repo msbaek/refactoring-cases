9cc2f07efa5091b5f306add6a20b8216e3b4b508
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-24 07:53:49 +0900

    usage first - add failing (high level) test

---
+++ src/test/java/pe/msbaek/rfcases/tdd/usagefirst/ShoppingBasketTest.java
@@ -0,0 +1,25 @@
+package pe.msbaek.rfcases.tdd.usagefirst;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ShoppingBasketTest {
+    @DisplayName("총 금액이 $100 초과 시 5% 할인 제공")
+    @Test
+    void total_over_100_gives_five_percent_discount() {
+        ShoppingBasket basket = new ShoppingBasket();
+        BasketItem itemA = new BasketItem("A", BigDecimal.valueOf(10));
+        BasketItem itemB = new BasketItem("B", BigDecimal.valueOf(25));
+        BasketItem itemC = new BasketItem("C", BigDecimal.valueOf(9.99));
+        basket.add(itemA, 5);
+        basket.add(itemB, 2);
+        basket.add(itemC, 6);
+        assertThat(basket.getQuantity("C")).isEqualTo(6);
+        assertThat(basket.calculateTotal().setScale(2, RoundingMode.HALF_UP)).isEqualTo(151.94);
+    }
+}
