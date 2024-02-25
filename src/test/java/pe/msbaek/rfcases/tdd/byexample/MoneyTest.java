df9e13c74d8fec901bb8c2f252f9f308794f397c
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-25 18:40:57 +0900

    by exam. - add failing test testEquality

--- src/test/java/pe/msbaek/rfcases/tdd/byexample/MoneyTest.java
+++ src/test/java/pe/msbaek/rfcases/tdd/byexample/MoneyTest.java
@@ -16,11 +16,16 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class MoneyTest {
     @Test
-    public void testMultiplication() {
+    void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertThat(product.amount()).isEqualTo(10);
         product = five.times(3);
         assertThat(product.amount()).isEqualTo(15);
+    }
+
+    @Test
+    void testEquality() {
+        assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
     }
 }
