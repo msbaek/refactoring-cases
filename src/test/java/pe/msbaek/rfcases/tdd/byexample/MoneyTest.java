e88e32caf6ac2358e0f8aa07a66606661c93a5d2
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-25 16:49:58 +0900

    by exam. - add more simple failing test

--- src/test/java/pe/msbaek/rfcases/tdd/byexample/MoneyTest.java
+++ src/test/java/pe/msbaek/rfcases/tdd/byexample/MoneyTest.java
@@ -1,7 +1,19 @@
 package pe.msbaek.rfcases.tdd.byexample;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * $5+ 10CHF=$10(환율이 2:1일 경우)
+ *
+ * $5 × 2 = $10
  */
 public class MoneyTest {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertThat(five.amount()).isEqualTo(10);
+    }
 }
