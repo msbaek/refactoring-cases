03758fb6af02f1fec425a74a704a4f7f5a906b6c
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-25 16:51:38 +0900

    by exam. - add more test scenario
    - 생각나는 경우들을 추가하고 현재의 일에 집중

--- src/test/java/pe/msbaek/rfcases/tdd/byexample/MoneyTest.java
+++ src/test/java/pe/msbaek/rfcases/tdd/byexample/MoneyTest.java
@@ -8,6 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
  * $5+ 10CHF=$10(환율이 2:1일 경우)
  *
  * $5 × 2 = $10
+ * Dollar 부작용(Value Object)
+ * BigDecimal 도입
  */
 public class MoneyTest {
     @Test
