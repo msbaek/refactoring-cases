a53385a00f8aa0a59b65c68126a5d9fd1ee8ba7a
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-25 18:18:47 +0900

    by exam. - mark test done

--- src/test/java/pe/msbaek/rfcases/tdd/byexample/MoneyTest.java
+++ src/test/java/pe/msbaek/rfcases/tdd/byexample/MoneyTest.java
@@ -7,7 +7,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * $5+ 10CHF=$10(환율이 2:1일 경우)
  *
- * $5 × 2 = $10
+ * X $5 × 2 = $10
+ *
  * Dollar 부작용(Value Object)
  * BigDecimal 도입
  */
