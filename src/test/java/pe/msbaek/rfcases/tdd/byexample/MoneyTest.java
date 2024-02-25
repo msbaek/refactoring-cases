61b3efbffa0f09e4a5dded2728de0144440f489f
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-25 20:15:34 +0900

    by exam. - testEquality를 확대

--- src/test/java/pe/msbaek/rfcases/tdd/byexample/MoneyTest.java
+++ src/test/java/pe/msbaek/rfcases/tdd/byexample/MoneyTest.java
@@ -37,5 +37,7 @@ public class MoneyTest {
     void testEquality() {
         assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
         assertThat(new Dollar(6)).isNotEqualTo(new Dollar(5));
+        assertThat(new Franc(5)).isEqualTo(new Franc(5));
+        assertThat(new Franc(6)).isNotEqualTo(new Franc(5));
     }
 }
