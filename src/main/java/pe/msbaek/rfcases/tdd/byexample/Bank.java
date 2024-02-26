cf3ec907ac9b759b2c579473f1ed9de5577bd0fb
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-26 15:19:13 +0900

    by exam. - rf. extract method & move instance method

--- src/main/java/pe/msbaek/rfcases/tdd/byexample/Bank.java
+++ src/main/java/pe/msbaek/rfcases/tdd/byexample/Bank.java
@@ -3,7 +3,6 @@ package pe.msbaek.rfcases.tdd.byexample;
 public class Bank {
     public Money reduce(Expression source, String to) {
         Sum sum = (Sum) source;
-        int amount = sum.augend.amount + sum.addend.amount;
-        return new Money(amount, to);
+        return sum.reduce(to);
     }
 }
