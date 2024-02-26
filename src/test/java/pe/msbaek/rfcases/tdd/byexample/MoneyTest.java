e960b4407660262b2c73bc00b08395db7fe50485
Myeongseok Baek <codetemplate@hanmail.net>
2024-02-26 15:07:27 +0900

    by exam. - change test for intention

--- src/test/java/pe/msbaek/rfcases/tdd/byexample/MoneyTest.java
+++ src/test/java/pe/msbaek/rfcases/tdd/byexample/MoneyTest.java
@@ -1,6 +1,9 @@
 package pe.msbaek.rfcases.tdd.byexample;
 
+import org.apache.commons.lang3.math.Fraction;
 import org.junit.jupiter.api.Test;
+
+import javax.sound.midi.Soundbank;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -59,7 +62,10 @@ public class MoneyTest {
 
     @Test
     void testSimpleAddition() {
-        Money sum = Money.dollar(5).plus(Money.dollar(5));
-        assertThat(sum).isEqualTo(Money.dollar(10));
+        Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        Money reduced = bank.reduce(sum, "USD");
+        assertThat(reduced).isEqualTo(Money.dollar(10));
     }
 }
