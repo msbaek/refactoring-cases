7da692b2a7ac06634807d80390c8e73fa3de9779
Myeongseok Baek <codetemplate@hanmail.net>
2024-03-27 14:44:11 +0900

    refactor-stream apply test data builder

--- src/test/java/pe/msbaek/rfcases/refactoring_stream/ConveyorRegistryTest.java
+++ src/test/java/pe/msbaek/rfcases/refactoring_stream/ConveyorRegistryTest.java
@@ -5,38 +5,87 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ItemOptionBuilder {
+    private Long id;
+    private Long weight;
+
+    public ItemOptionBuilder id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public ItemOptionBuilder weight(Long weight) {
+        this.weight = weight;
+        return this;
+    }
+
+    public ItemOption build() {
+        return new ItemOption() {
+            public Long getId() {
+                return id;
+            }
+
+            public Long getWeight() {
+                return weight;
+            }
+        };
+    }
+}
+
+class ShippingItemBuilder {
+    private Long itemId;
+    private Long qty;
+
+    public ShippingItemBuilder itemId(Long itemId) {
+        this.itemId = itemId;
+        return this;
+    }
+
+    public ShippingItemBuilder qty(Long qty) {
+        this.qty = qty;
+        return this;
+    }
+
+    public ShippingItem build() {
+        return new ShippingItem() {
+            public Long getItemId() {
+                return itemId;
+            }
+
+            public Long getQty() {
+                return qty;
+            }
+        };
+    }
+}
 
 class ConveyorRegistryTest {
     @Test
     public void testCalculateTotalWeight() {
         ConveyorRegistry conveyorRegistry = new ConveyorRegistry();
-
         List<ItemOption> itemOptions = Arrays.asList(
-                new ItemOption() {
-                    public Long getId() { return 1L; }
-                    public Long getWeight() { return 2L; }
-                },
-                new ItemOption() {
-                    public Long getId() { return 2L; }
-                    public Long getWeight() { return 3L; }
-                }
+                anItemOption().id(1L).weight(2L).build(),
+                anItemOption().id(2L).weight(3L).build()
         );
 
         List<ShippingItem> shippingItems = Arrays.asList(
-                new ShippingItem() {
-                    public Long getItemId() { return 1L; }
-                    public Long getQty() { return 2L; }
-                },
-                new ShippingItem() {
-                    public Long getItemId() { return 2L; }
-                    public Long getQty() { return 3L; }
-                }
+                aShippingItem().itemId(1L).qty(2L).build(),
+                aShippingItem().itemId(2L).qty(3L).build()
         );
+        Long expectedTotalWeight = 2L * 2L + 3L * 3L;
 
-        Long expectedTotalWeight = 2L*2L + 3L*3L;
         Long actualTotalWeight = conveyorRegistry.calculateTotalWeight(itemOptions, shippingItems);
 
         assertEquals(expectedTotalWeight, actualTotalWeight);
+    }
+
+    private ShippingItemBuilder aShippingItem() {
+        return new ShippingItemBuilder();
+    }
+
+    private ItemOptionBuilder anItemOption() {
+        return new ItemOptionBuilder();
     }
 }
