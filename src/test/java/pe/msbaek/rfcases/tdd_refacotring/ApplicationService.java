35cdad8205217b9babd1053469b0a36f670fc6c9
Myeongseok Baek <codetemplate@hanmail.net>
2024-04-08 12:03:37 +0900

    param-obj replace record to class

--- src/test/java/pe/msbaek/rfcases/tdd_refacotring/ApplicationService.java
+++ src/test/java/pe/msbaek/rfcases/tdd_refacotring/ApplicationService.java
@@ -2,15 +2,25 @@ package pe.msbaek.rfcases.tdd_refacotring;
 
 import java.util.List;
 
-public record ApplicationService(LoadWarehousePort loadWarehousePort1, CarrierPort carrierPort1,
-                                 LoadInventoryLpnPort loadInventoryLpnPort1) {
+public final class ApplicationService {
+    private final LoadWarehousePort loadWarehousePort1;
+    private final CarrierPort carrierPort1;
+    private final LoadInventoryLpnPort loadInventoryLpnPort1;
+
+    public ApplicationService(LoadWarehousePort loadWarehousePort1, CarrierPort carrierPort1,
+                              LoadInventoryLpnPort loadInventoryLpnPort1) {
+        this.loadWarehousePort1 = loadWarehousePort1;
+        this.carrierPort1 = carrierPort1;
+        this.loadInventoryLpnPort1 = loadInventoryLpnPort1;
+    }
+
     InventoryTransferOrder getInventoryTransferOrder(InventoryTransferOrderRequest request) {
         // when // todo: application 서비스로 extract
         final List<ItemToTransfer> itemsToTransfer = request.getItemsToCreate();
-        final Warehouse toWarehouse = loadWarehousePort1().getBy(request.toWarehouseId());
-        final Warehouse fromWarehouse = loadWarehousePort1().getBy(request.fromWarehouseId());
-        final Carrier carrier = carrierPort1().getBy(request.carrierId());
-        final List<InventoryWarehouse> inventoryWarehouses = loadInventoryLpnPort1().findBy(request.fromWarehouseId(), itemsToTransfer.stream().map(ItemToTransfer::itemId).toList());
+        final Warehouse toWarehouse = loadWarehousePort1.getBy(request.toWarehouseId());
+        final Warehouse fromWarehouse = loadWarehousePort1.getBy(request.fromWarehouseId());
+        final Carrier carrier = carrierPort1.getBy(request.carrierId());
+        final List<InventoryWarehouse> inventoryWarehouses = loadInventoryLpnPort1.findBy(request.fromWarehouseId(), itemsToTransfer.stream().map(ItemToTransfer::itemId).toList());
 
         //todo: 도메인 서비스로 분리
         final Shipping shipping = createShipping(itemsToTransfer, inventoryWarehouses);
