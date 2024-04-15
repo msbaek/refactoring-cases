54f0981fb80df553271e8c22c1b8a71535d219c6
Myeongseok Baek <codetemplate@hanmail.net>
2024-04-16 07:39:42 +0900

    first-class-collection add required files

---
+++ src/main/java/pe/msbaek/rfcases/valueobject/FirstClassCollectionService.java
@@ -0,0 +1,60 @@
+package pe.msbaek.rfcases.valueobject;
+
+import lombok.Getter;
+
+import java.util.Collection;
+import java.util.List;
+
+public class FirstClassCollectionService {
+    private CompanyPort companyPort;
+
+    public void createLocation(final List<LocationRequest> locationRequests) {
+        final Collection<Company> companies = companyPort.loadAll();
+
+        locationRequests.stream()
+                .map(locationRequest -> mapToCreateLocationCommand(locationRequest))
+                .forEach(createLocationCommand -> {
+                    final Company company = getCompany(companies, createLocationCommand.companyCode());
+                    company.createOrUpdateLocation(createLocationCommand);
+                });
+
+        companyPort.saveAll(companies);
+    }
+
+    private Company.CreateLocationCommand mapToCreateLocationCommand(final LocationRequest locationRequest) {
+        return Company.CreateLocationCommand.of(locationRequest);
+    }
+
+    private Company getCompany(final Collection<Company> companies, final String companyCode) {
+        return companies.stream()
+                .filter(company1 -> company1.getCode().equals(companyCode))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Company not found"));
+    }
+}
+
+record LocationRequest() {
+
+}
+
+@Getter
+class Company {
+
+    private String code;
+
+    public void createOrUpdateLocation(CreateLocationCommand createLocationCommand) {
+        throw new UnsupportedOperationException("Company::createOrUpdateLocation not implemented yet");
+    }
+    record CreateLocationCommand(String companyCode) {
+        public static CreateLocationCommand of(LocationRequest locationRequest) {
+            throw new UnsupportedOperationException("CreateLocationCommand::of not implemented yet");
+        }
+    }
+
+}
+
+interface CompanyPort {
+
+    Collection<Company> loadAll();
+    void saveAll(Collection<Company> companies);
+}
