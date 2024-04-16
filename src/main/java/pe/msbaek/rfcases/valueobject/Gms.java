0c4b4c4c7e2fb1b1f05514a20245c95c03a2e4d0
Myeongseok Baek <codetemplate@hanmail.net>
2024-04-16 11:34:32 +0900

    first-class-collection extract methods - inline companies(), companies

--- src/main/java/pe/msbaek/rfcases/valueobject/Gms.java
+++ src/main/java/pe/msbaek/rfcases/valueobject/Gms.java
@@ -3,23 +3,16 @@ package pe.msbaek.rfcases.valueobject;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 import static java.util.stream.Collectors.toMap;
 
 // first class collection
 public final class Gms {
-    private final Collection<Company> companies;
     private final Map<String, Company> companyMap;
 
     public Gms(Collection<Company> companies) {
-        this.companies = companies;
-        companyMap = companies().stream()
+        companyMap = companies.stream()
                 .collect(toMap(Company::getCode, company -> company));
-    }
-
-    public Collection<Company> companies() {
-        return companies;
     }
 
     void createOrUpdteLocations(List<LocationRequest> locationRequests) {
