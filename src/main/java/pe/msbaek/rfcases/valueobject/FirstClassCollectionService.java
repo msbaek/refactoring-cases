9f7efd4d3d185e644954970884b7c57b1df70370
Myeongseok Baek <codetemplate@hanmail.net>
2024-04-16 11:31:57 +0900

    first-class-collection extract methods - move instance method to Gms

--- src/main/java/pe/msbaek/rfcases/valueobject/FirstClassCollectionService.java
+++ src/main/java/pe/msbaek/rfcases/valueobject/FirstClassCollectionService.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 import static java.util.stream.Collectors.toMap;
 
@@ -13,21 +12,9 @@ public class FirstClassCollectionService {
 
     public void createLocation(final List<LocationRequest> locationRequests) {
         final Collection<Company> companies = companyPort.loadAll();
-        createOrUpdteLocations(locationRequests, new Gms(companies));
+        new Gms(companies).createOrUpdteLocations(locationRequests);
 
         companyPort.saveAll(companies);
-    }
-
-    private void createOrUpdteLocations(List<LocationRequest> locationRequests, Gms gms) {
-        Map<String, Company> companyMap = gms.companies().stream()
-                .collect(toMap(Company::getCode, company -> company));
-
-        for (LocationRequest locationRequest : locationRequests) {
-            Company.CreateLocationCommand createLocationCommand = Company.CreateLocationCommand.of(locationRequest);
-            // final Company company = getCompany(companies, createLocationCommand.companyCode());
-            final Company company = companyMap.get(createLocationCommand.companyCode());
-            company.createOrUpdateLocation(createLocationCommand);
-        }
     }
 }
 
@@ -37,7 +24,6 @@ record LocationRequest() {
 
 @Getter
 class Company {
-
     private String code;
 
     public void createOrUpdateLocation(CreateLocationCommand createLocationCommand) {
@@ -52,7 +38,6 @@ class Company {
 }
 
 interface CompanyPort {
-
     Collection<Company> loadAll();
     void saveAll(Collection<Company> companies);
 }
--- src/main/java/pe/msbaek/rfcases/valueobject/Gms.java
+++ src/main/java/pe/msbaek/rfcases/valueobject/Gms.java
@@ -1,7 +1,11 @@
 package pe.msbaek.rfcases.valueobject;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+
+import static java.util.stream.Collectors.toMap;
 
 public final class Gms {
     private final Collection<Company> companies;
@@ -12,5 +16,17 @@ public final class Gms {
 
     public Collection<Company> companies() {
         return companies;
+    }
+
+    void createOrUpdteLocations(List<LocationRequest> locationRequests) {
+        Map<String, Company> companyMap = companies().stream()
+                .collect(toMap(Company::getCode, company -> company));
+
+        for (LocationRequest locationRequest : locationRequests) {
+            Company.CreateLocationCommand createLocationCommand = Company.CreateLocationCommand.of(locationRequest);
+            // final Company company = getCompany(companies, createLocationCommand.companyCode());
+            final Company company = companyMap.get(createLocationCommand.companyCode());
+            company.createOrUpdateLocation(createLocationCommand);
+        }
     }
 }
