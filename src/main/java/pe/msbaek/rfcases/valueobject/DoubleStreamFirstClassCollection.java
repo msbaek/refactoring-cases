3772c9640fd5043822c856e577432cc142ddd011
Myeongseok Baek <codetemplate@hanmail.net>
2024-06-15 11:38:16 +0900

    introduce parameter object

--- src/main/java/pe/msbaek/rfcases/valueobject/DoubleStreamFirstClassCollection.java
+++ src/main/java/pe/msbaek/rfcases/valueobject/DoubleStreamFirstClassCollection.java
@@ -26,20 +26,23 @@ public class DoubleStreamFirstClassCollection {
     private List<CartAdditionalItem> getCartAdditionalItems(final List<AdditionalProduct> additionalProducts, final List<CartAdditionalItemDto> cartAdditionalItemDtos) {
         List<CartAdditionalItem> list = new ArrayList<>();
         for (AdditionalProduct additionalProduct : additionalProducts) {
-            List<CartAdditionalItem> c = mapToAdditionalItems(cartAdditionalItemDtos, additionalProduct);
+            List<CartAdditionalItem> c = mapToAdditionalItems(new CartItemList(cartAdditionalItemDtos), additionalProduct);
             list.addAll(c);
         }
         return list;
     }
 
-    private List<CartAdditionalItem> mapToAdditionalItems(List<CartAdditionalItemDto> cartAdditionalItemDtos, AdditionalProduct additionalProduct) {
+    private List<CartAdditionalItem> mapToAdditionalItems(CartItemList cartItemList, AdditionalProduct additionalProduct) {
         List<CartAdditionalItem> c = new ArrayList<>();
-        for (CartAdditionalItemDto cartAdditionalItemDto : cartAdditionalItemDtos) {
+        for (CartAdditionalItemDto cartAdditionalItemDto : cartItemList.cartAdditionalItemDtos()) {
             if (additionalProduct.getId().productNo().equals(cartAdditionalItemDto.productNo()) && additionalProduct.getId().fanClubProductNo().equals(cartAdditionalItemDto.fanClubProductNo())) {
                 CartAdditionalItem cartAdditionalItem = new CartAdditionalItem(additionalProduct.getId().productNo(), additionalProduct.getId().fanClubProductNo(), cartAdditionalItemDto.qty());
                 c.add(cartAdditionalItem);
             }
         }
         return c;
+    }
+
+    private static record CartItemList(List<CartAdditionalItemDto> cartAdditionalItemDtos) {
     }
 }
