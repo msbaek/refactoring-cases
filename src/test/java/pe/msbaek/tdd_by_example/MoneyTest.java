927fedbc0f3523c108d22e2323852fd647434602
Myeongseok Baek <codetemplate@hanmail.net>
2024-12-31 16:39:49 +0900

    1.1 add todo list

---
+++ src/test/java/pe/msbaek/tdd_by_example/MoneyTest.java
@@ -0,0 +1,6 @@
+package pe.msbaek.tdd_by_example;
+
+/// - [ ] $5 + 10CHF = $10(환율이 2:1일 경우)
+/// - [O] $5 × 2 = $10
+public class MoneyTest {
+}
---
+++ src/test/java/pe/msbaek/tdd_by_example/README.md
@@ -0,0 +1,5 @@
+# TDD By Example
+
+## 1장. 다중 통화를 지원하는 Money 객체
+
+### 1.1 todo list 추가하기
