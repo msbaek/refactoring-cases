8752645e5d439d4732514cbacc8a237048964f64
Myeongseok Baek <codetemplate@hanmail.net>
2024-12-31 16:50:35 +0900

    1.2 add failing test - testMultiplication

--- src/test/java/pe/msbaek/tdd_by_example/MoneyTest.java
+++ src/test/java/pe/msbaek/tdd_by_example/MoneyTest.java
@@ -1,6 +1,16 @@
 package pe.msbaek.tdd_by_example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 /// - [ ] $5 + 10CHF = $10(환율이 2:1일 경우)
 /// - [O] $5 × 2 = $10
 public class MoneyTest {
+    @DisplayName("어떤 금액(주가)을 어떤 수(주식의 수)로 곱한 금액을 결과로 얻을 수 있어야 한다")
+    @Test
+    void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertThat(five.amount).isEqualTo(10);
+    }
 }
--- src/test/java/pe/msbaek/tdd_by_example/README.md
+++ src/test/java/pe/msbaek/tdd_by_example/README.md
@@ -3,3 +3,14 @@
 ## 1장. 다중 통화를 지원하는 Money 객체
 
 ### 1.1 todo list 추가하기
+
+### 1.2 add failing test - testMultiplication
+```Java
+    @DisplayName("어떤 금액(주가)을 어떤 수(주식의 수)로 곱한 금액을 결과로 얻을 수 있어야 한다")
+    @Test
+    void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertThat(five.amount).isEqualTo(10);
+    }
+```
