88bb62607e89617779864926284fe4ffc7e4a8c1
Myeongseok Baek <codetemplate@hanmail.net>
2025-01-01 21:53:07 +0900

    12.3 introduce metaphor - Expression, Bank

--- src/test/java/pe/msbaek/tdd_by_example/MoneyTest.java
+++ src/test/java/pe/msbaek/tdd_by_example/MoneyTest.java
@@ -57,7 +57,10 @@ public class MoneyTest {
 
     @Test
     void testSimpleAddtion() {
-        Money sum = Money.dollar(5).plus(Money.dollar(5));
-        assertThat(sum).isEqualTo(Money.dollar(10));
+        final Money five = Money.dollar(5);
+        Expression sum = five.plus(five);
+        Bank bank = new Bank();
+        final Money reduced = bank.reduce(sum, "USD");
+        assertThat(reduced).isEqualTo(Money.dollar(10));
     }
 }
