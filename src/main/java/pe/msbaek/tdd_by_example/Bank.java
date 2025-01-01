6faf557b51c793feb9d6b357630a0e1c7f0b44c3
Myeongseok Baek <codetemplate@hanmail.net>
2025-01-01 22:19:40 +0900

    13.5.2 move method

--- src/main/java/pe/msbaek/tdd_by_example/Bank.java
+++ src/main/java/pe/msbaek/tdd_by_example/Bank.java
@@ -3,11 +3,6 @@ package pe.msbaek.tdd_by_example;
 public class Bank {
     public Money reduce(final Expression source, final String to) {
         Sum sum = (Sum) source;
-        return reduce(to, sum);
-    }
-
-    private Money reduce(final String to, final Sum sum) {
-        int amount = sum.augend.amount + sum.addend.amount;
-        return new Money(amount, to);
+        return sum.reduce(to);
     }
 }
