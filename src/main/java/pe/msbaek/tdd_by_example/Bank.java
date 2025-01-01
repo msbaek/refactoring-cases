8beacb0281b81afc98ff0d3e1fa36e72b70344d9
Myeongseok Baek <codetemplate@hanmail.net>
2025-01-01 21:56:59 +0900

    12.5 make it work by fake it

--- src/main/java/pe/msbaek/tdd_by_example/Bank.java
+++ src/main/java/pe/msbaek/tdd_by_example/Bank.java
@@ -2,6 +2,6 @@ package pe.msbaek.tdd_by_example;
 
 public class Bank {
     public Money reduce(final Expression source, final String to) {
-        return null;
+        return Money.dollar(10);
     }
 }
